a1836b0304a30d77f23bea25e4c2d0d4ad505532
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 21:37:25 +0530

    snakeproblem.cs

---
+++ snakeproblem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public static class snakeproblem
+    {
+        public static void displaysnakeladder()
+        {
+            int player1 = 0;
+
+            Random random = new Random();
+            int dice1 = random.Next(7);
+
+            Console.WriteLine("Random number : " + dice1);
+           
+
+
+
+        }
+    }
+}
