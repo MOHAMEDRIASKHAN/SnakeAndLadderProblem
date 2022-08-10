08f4cf5dc26a3e15bc839edb174d75f7311dcd8c
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 21:41:23 +0530

    WinningPosition.cs

---
+++ WinningPosition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public static class WinningPosition
+    {
+        public static void WinOrNot()
+        {
+            const int zeroposition = 0;
+            const int nonzeroposition = 1;
+
+            Random random = new Random();
+            int dice = random.Next(2);
+            Console.WriteLine("Dice : " + dice);
+
+            if (dice == nonzeroposition)
+            {
+                Console.WriteLine("Player reached winning point");
+            }
+            else if (dice == zeroposition)
+            {
+                Console.WriteLine("Player restart the game");
+}
+        }
+    }
+}
