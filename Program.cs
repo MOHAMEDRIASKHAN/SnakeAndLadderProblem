71ecad17cd187badda5f370233f0d2b7243ea920
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 21:31:16 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace SnakeLadderProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //  snakeproblem.displaysnakeladder();
+            //CheckTheOption.checkingOption();
+            // WinningPosition.WinOrNot();
+            //WinningMoment.DisplayWinningMoment();
+           // ReportWinningMoment.DisplayReportWinningMoment();
+        }
+    }
+}
