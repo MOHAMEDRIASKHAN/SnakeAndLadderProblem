27ddc0aec8e7523b5df4d870a021983c878950ed
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 21:42:50 +0530

    WinningMoment.cs

---
+++ WinningMoment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public static class WinningMoment
+    {
+        public static void DisplayWinningMoment()
+        {
+            const int WinPoint = 1;
+
+            Random r = new Random();
+            int dice = r.Next(7);
+
+            Console.WriteLine("Dice Number : " +dice);
+
+            if (dice == WinPoint)
+            {
+                Console.WriteLine("Player Win the Game");
+            }
+            else
+            {
+                Console.WriteLine("Player stay the same position");
+            }
+
+        }
+    }
+}
