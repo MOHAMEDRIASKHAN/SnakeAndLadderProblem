ee789a3559ff5a49cf07611c5dc115997659dff1
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-10 21:44:13 +0530

    ReportWinningMoment.cs

---
+++ ReportWinningMoment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public static class ReportWinningMoment
+    {
+        public static void DisplayReportWinningMoment()
+        {
+            const int WinPoint = 1;
+
+            Random r = new Random();
+            int dice = r.Next(7);
+
+            Console.WriteLine("Dice Number : " + dice);
+
+            if (dice == WinPoint)
+            {
+                Console.WriteLine("Player Win the Game");
+            }
+            else
+            {
+                Console.WriteLine("Wait for Win");
+            }
+        }
+    }
+}
